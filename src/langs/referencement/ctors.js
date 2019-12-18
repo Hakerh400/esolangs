bba584a5a711034f4ffcbc26af84383c458c9c72
Thomas <hakerh403@gmail.com>
2019-12-18 04:02:05 +0100

    commit

--- src/langs/referencement/ctors.js
+++ src/langs/referencement/ctors.js
@@ -12,6 +12,7 @@ class Expression extends Base{
   sliced = null;
 
   get type(){ O.virtual('type'); }
+  shallowSlice(){ O.virtual('shallowSlice'); }
   replace(expr1, expr2){ O.virtual('replace'); }
   toStr(){ O.virtual('toStr'); }
 
@@ -20,10 +21,16 @@ class Expression extends Base{
 
     while(stack.length !== 0){
       const expr = stack.pop();
+
+      const result = func(expr);
+      if(result === 0) continue;
+
+      if(result instanceof Expression){
+        stack.push(result);
+        continue;
+      }
+
       const {type} = expr;
-
-      if(func(expr) === 0) continue;
-
       if(type === 1) stack.push(expr.expr);
       else if(type === 2) stack.push(expr.expr2, expr.expr1);
     }
@@ -49,20 +56,33 @@ class Expression extends Base{
 
       if(mode === 0){
         stack.push(1, expr);
-        if(type === 1) stack.push(0, expr.expr);
-        else if(type === 2) stack.push(0, expr.expr2, 0, expr.expr1);
-        if(mode === 0) continue;
+
+        if(type === 1){
+          stack.push(0, expr.expr);
+          continue;
+        }
+
+        if(type === 2){
+          stack.push(0, expr.expr2, 0, expr.expr1);
+          continue;
+        }
+
+        O.noimpl(expr.constructor.name);
       }
 
       if(type === 1){
         const exprNew = new Abstraction(expr.ident, expr.byRef, expr.expr.sliced);
-        exprNew.x = expr.x;
-        exprNew.y = expr.y;
+        exprNew.params = expr.params.slice();
         expr.sliced = exprNew;
         continue;
       }
 
-      expr.sliced = new Invocation(expr.expr1.sliced, expr.expr2.sliced);
+      if(type === 2){
+        expr.sliced = new Invocation(expr.expr1.sliced, expr.expr2.sliced);
+        continue;
+      }
+
+      O.noimpl(expr.constructor.name);
     }
 
     return this.sliced;
@@ -103,14 +123,18 @@ class Identifier extends Expression{
 
   get type(){ return 0; }
 
+  shallowSlice(){
+    return new Identifier(this.name);
+  }
+
   toStr(){
     return this.name;
   }
 }
 
 class Abstraction extends Expression{
-  x = null;
-  y = null;
+  params = [null, null, null];
+  wasFancy = 0;
 
   constructor(ident, byRef, expr){
     super();
@@ -123,19 +147,24 @@ class Abstraction extends Expression{
 
   get type(){ return 1; }
 
+  shallowSlice(){
+    return new Abstraction(this.ident, this.byRef, this.expr);
+  }
+
   replace(expr1, expr2){
     this.expr = expr2;
     return expr2.parent = this;
   }
 
   toStr(){
+    const {params} = this;
     const arr = [];
 
-    if(this.x !== null) arr.push(`${this.x}-`);
-    arr.push(this.ident);
-    if(this.y !== null) arr.push(`-${this.y}`);
+    if(params[0] !== null) arr.push(`${params[0]}-`);
+    arr.push(`${this.byRef ? '&' : ''}${this.ident}`);
+    if(params[1] !== null) arr.push(`-${params[1]}`);
 
-    arr.push(`${this.byRef ? ':' : '.'} `);
+    arr.push('. ');
     arr.push(this.expr);
 
     return arr;
@@ -153,6 +182,10 @@ class Invocation extends Expression{
   }
 
   get type(){ return 2; }
+
+  shallowSlice(){
+    return new Invocation(this.expr1, this.expr2);
+  }
 
   replace(expr1, expr2){
     if(expr1 === this.expr1) this.expr1 = expr2;
@@ -192,8 +225,103 @@ class NativeFunction extends Expression{
 
   get type(){ return 3; }
 
+  shallowSlice(){
+    return new NativeFunction(this.id);
+  }
+
   toStr(){
     return `[${this.id}]`;
+  }
+}
+
+class FancyFunction extends Expression{
+  constructor(args, exprs){
+    super();
+    this.args = args;
+    this.exprs = exprs;
+
+    if(args.length === 0)
+      args.push(new FancyArgument('#'));
+  }
+
+  get type(){ return 5; }
+
+  shallowSlice(){
+    return new FancyFunction(this.args, this.exprs);
+  }
+}
+
+class FancyArgument extends Base{
+  constructor(ident, byRef=0){
+    super();
+    this.ident = ident;
+    this.byRef = byRef;
+  }
+}
+
+class FancyCall extends Expression{
+  constructor(func, args){
+    super();
+    this.func = func;
+    this.args = args;
+
+    if(args.length === 0)
+      args.push(new Identifier('#0'));
+  }
+
+  get type(){ return 5; }
+
+  shallowSlice(){
+    return new FancyCall(this.func, this.args);
+  }
+
+  toStr(){
+    const {args} = this;
+    const arr = ['('];
+
+    for(let i = 0; i !== args.length; i++){
+      if(i !== 0) arr.push(', ');
+      arr.push(args[i]);
+    }
+
+    arr.push(')');
+    return arr;
+  }
+}
+
+class VarDeclaration extends Expression{
+  constructor(ident, expr){
+    super();
+    this.ident = ident;
+    this.expr = expr;
+  }
+
+  get type(){ return 6; }
+
+  shallowSlice(){
+    return new VarDeclaration(this.ident, this.expr);
+  }
+
+  toStr(){
+    return [this.ident, ' := ', this.expr];
+  }
+}
+
+class Assignment extends Expression{
+  constructor(ident, expr){
+    super();
+    this.ident = ident;
+    this.expr = expr;
+  }
+
+  get type(){ return 7; }
+
+  shallowSlice(){
+    return new Assignment(this.ident, this.expr);
+  }
+
+  toStr(){
+    return [this.ident, ' = ', this.expr];
   }
 }
 
@@ -204,4 +332,9 @@ module.exports = {
   Abstraction,
   Invocation,
   NativeFunction,
+  FancyFunction,
+  FancyArgument,
+  FancyCall,
+  VarDeclaration,
+  Assignment,
 };
--- src/langs/referencement/engine.js
+++ src/langs/referencement/engine.js
@@ -6,6 +6,12 @@ const O = require('omikron');
 const esolangs = require('../..');
 const cs = require('./ctors');
 
+const DEBUG = 0;
+const DEBUG_NATIVE_FUNCS = 0;
+
+const DUMP_GLOBAL_EXPR = 0;
+const DUMP_FILE = 'C:/Users/Thomas/Downloads/1.txt';
+
 class Engine{
   constructor(parsed, input){
     this.parsed = parsed;
@@ -17,9 +23,124 @@ class Engine{
     const {parsed, input} = this;
     const io = new O.IO(input, 0, 1);
 
-    let expr = parsed;
+    let globalExpr = parsed;
 
-    expr.iter(expr => {
+    const avail = index => {
+      const obj = O.obj();
+
+      globalExpr.iter(expr => {
+        if(expr.type !== 1) return;
+        if(expr.params[index] === null) return;
+        obj[expr.params[index]] = 1;
+      });
+
+      for(let i = 0;; i++)
+        if(!(i in obj)) return i;
+    };
+
+    const replace = (e1, e2) => {
+      const {parent} = e1;
+
+      if(parent !== null){
+        parent.replace(e1, e2);
+      }else{
+        globalExpr = e2;
+        e2.parent = null;
+      }
+
+      return e2;
+    };
+
+    const logExpr = (expr=globalExpr) => {
+      let str = expr.toString();
+
+      if(str.length >= 200)
+        str = `${str.slice(0, 196)}...`;
+
+      log(str);
+    };
+
+    globalExpr.iter(expr => {
+      if(expr instanceof cs.FancyFunction){
+        const {args, exprs} = expr;
+        const len = exprs.length;
+
+        let e = O.last(exprs);
+
+        if(len !== 1){
+          let e1 = new cs.Identifier('#nop');
+
+          for(let i = 0; i !== len - 1; i++)
+            e1 = new cs.Invocation(e1, exprs[i]);
+
+          e = new cs.Invocation(new cs.Invocation(new cs.Identifier('#0'), e1), e);
+        }
+
+        for(let i = args.length - 1; i !== -1; i--){
+          const arg = args[i];
+          e = new cs.Abstraction(arg.ident, arg.byRef, e);
+          e.wasFancy = 1;
+        }
+
+        if(expr.parent === null){
+          e = new cs.Invocation(e, new cs.Identifier('#assign'));
+
+          e = new cs.Abstraction('#assign', 1, e);
+          e = new cs.Abstraction('#0', 1, e);
+          e = new cs.Abstraction('#nop', 1, e);
+
+          e = new cs.Invocation(e,
+            new cs.Invocation(
+              new cs.Abstraction('a', 1, new cs.Invocation(new cs.Identifier('a'), new cs.Identifier('a'))),
+              new cs.Abstraction('a', 1, new cs.Abstraction('b', 1, new cs.Invocation(new cs.Identifier('a'), new cs.Identifier('a')))),
+            ),
+          );
+
+          e = new cs.Invocation(e,
+            new cs.Abstraction('a', 1, new cs.Abstraction('b', 1, new cs.Identifier('b'))),
+          );
+        }
+
+        return replace(expr, e);
+      }
+
+      if(expr instanceof cs.FancyCall){
+        let e = expr.func;
+
+        for(const arg of expr.args)
+          e = new cs.Invocation(e, arg);
+
+        return replace(expr, e);
+      }
+
+      if(expr instanceof cs.VarDeclaration){
+        const exprNew = replace(expr, new cs.Invocation(
+          new cs.Invocation(new cs.Identifier('#assign'), new cs.Identifier(expr.ident)),
+          expr.expr,
+        ));
+
+        let e = exprNew;
+
+        while(e.parent !== null && (e.parent.type !== 1 || !e.parent.wasFancy))
+          e = e.parent;
+
+        replace(e, new cs.Invocation(
+          new cs.Abstraction(expr.ident, 0, e.shallowSlice()),
+          new cs.Identifier('#0'),
+        ));
+
+        return exprNew;
+      }
+
+      if(expr instanceof cs.Assignment){
+        return replace(expr, new cs.Invocation(
+          new cs.Invocation(new cs.Identifier('#assign'), new cs.Identifier(expr.ident)),
+          expr.expr,
+        ));
+      }
+    });
+
+    globalExpr.iter(expr => {
       if(expr.type !== 0) return;
 
       const ident = expr.name;
@@ -32,61 +153,35 @@ class Engine{
       }
     });
 
-    [2, 3, 1, 1, 1].forEach((n, i) => {
-      let func = new cs.NativeFunction(i);
+    [2, 3, 1, 1, 1].forEach((n, index) => {
+      let func = new cs.NativeFunction(index);
 
       for(let i = 0; i !== n; i++)
         func = new cs.Invocation(func, new cs.Identifier(O.sfcc(i + 97)));
 
       for(let i = n - 1; i !== -1; i--)
-        func = new cs.Abstraction(O.sfcc(i + 97), 1, func);
+        func = new cs.Abstraction(O.sfcc(i + 97), index !== 0 || i !== 1, func);
 
-      expr = new cs.Invocation(expr, func);
+      globalExpr = new cs.Invocation(globalExpr, func);
     });
 
-    const availX = expr => {
-      const xx = O.obj();
+    if(DUMP_GLOBAL_EXPR)
+      O.wfs(DUMP_FILE, globalExpr.toString());
 
-      expr.iter(expr => {
-        if(expr.type !== 1) return;
-        if(expr.x === null) return;
-        xx[expr.x] = 1;
-      });
+    while(globalExpr.type === 2){
+      let e = globalExpr;
 
-      for(let i = 0;; i++)
-        if(!(i in xx)) return i;
-    };
+      tco: if(e.type === 2){
+        const e1 = e.expr1;
+        if(e1.type !== 1) break tco;
 
-    const availY = expr => {
-      const yy = O.obj();
+        const e2 = e1.expr;
+        if(e2.type !== 0) break tco;
+        if(e2.name !== e1.ident) break tco;
 
-      expr.iter(expr => {
-        if(expr.type !== 1) return;
-        if(expr.y === null) return;
-        yy[expr.y] = 1;
-      });
-
-      for(let i = 0;; i++)
-        if(!(i in yy)) return i;
-    };
-
-    const replace = (e1, e2) => {
-      const {parent} = e1;
-
-      if(parent !== null){
-        parent.replace(e1, e2);
-      }else{
-        expr = e2;
-        e2.parent = null;
+        replace(globalExpr, e.expr2);
+        continue;
       }
-
-      return parent;
-    };
-
-    while(expr.type === 2){
-      log(expr.toString());
-
-      let e = expr;
 
       while(1){
         while(e.expr1.type === 2) e = e.expr1;
@@ -97,26 +192,30 @@ class Engine{
       const {parent, expr1, expr2} = e;
 
       if(expr1.type === 1){
-        expr1.x = null;
-        expr1.y = null;
+        if(DEBUG) logExpr(e);
 
-        if(expr2.x === null) expr2.x = availX(expr);
-        if(!expr1.byRef) expr2.y = null;
-        if(expr2.y === null) expr2.y = availY(expr);
+        expr1.params[0] = null;
+        expr1.params[1] = null;
+
+        if(expr2.params[0] === null) expr2.params[0] = avail(0);
+        if(!expr1.byRef) expr2.params[1] = null;
+        if(expr2.params[1] === null) expr2.params[1] = avail(1);
 
         const {ident} = expr1;
         const e1 = expr1.expr;
 
-        if(parent !== null){
-          parent.replace(e, e1);
-        }else{
-          expr = e1;
-          e1.parent = null;
-        }
+        replace(e, e1);
+        // if(parent !== null){
+        //   parent.replace(e, e1);
+        // }else{
+        //   globalExpr = e1;
+        //   e1.parent = null;
+        // }
 
         e1.iter(expr => {
           if(expr.type === 1 && expr.ident === ident) return 0;
           if(expr.type !== 0 || expr.name !== ident) return;
+
           replace(expr, expr2.slice());
         });
 
@@ -126,59 +225,86 @@ class Engine{
       const {id} = expr1;
 
       if(id === 0){
+        if(DEBUG) logExpr(e.parent);
+        if(DEBUG_NATIVE_FUNCS) log(`\n---> ASSIGN\n`);
+
         const e1 = expr2;
         const e2 = parent.expr2;
-        const {y} = e1;
+        const y = e1.params[1];
 
-        replace(parent, e2);
+        const z = avail(2);
+        const ident = new cs.Identifier(`{${z}}`);
+        const recInv = new cs.Invocation(ident.slice(), ident.slice());
+        const recFunc = new cs.Abstraction(ident.name, 1, recInv.slice());
+        recFunc.params[2] = z;
 
-        expr.iter(e => {
-          if(e.type !== 1 || e.y !== y) return;
-          replace(e, e2.slice());
+        e2.iter(expr => {
+          if(expr.type !== 1 || expr.params[1] !== y) return;
+
+          replace(expr, recInv);
+        });
+
+        const func = new cs.Abstraction(ident.name, 1, e2);
+        func.params[2] = z;
+
+        const inv = new cs.Invocation(recFunc, func);
+        replace(parent, inv);
+
+        globalExpr.iter(expr => {
+          if(expr === e2) return 0;
+          if(expr.type !== 1 || expr.params[1] !== y) return;
+
+          replace(expr, inv.slice());
         });
 
         continue;
       }
 
       if(id === 1){
+        if(DEBUG) logExpr(e.parent.parent);
+        if(DEBUG_NATIVE_FUNCS) log(`\n---> COMPARE\n`);
+
         const p = parent.parent;
         const e1 = expr2;
         const e2 = parent.expr2;
         const e3 = p.expr2;
 
-        replace(
-          p, e1.x === e2.x ?
-            new cs.Invocation(e3, e3.slice()) :
-            e3
-        );
+        replace(p, e1.params[0] === e2.params[0] ? new cs.Invocation(e3, e3.slice()) : e3);
+
         continue;
       }
 
       if(id === 2){
-        replace(
-          e, io.read() ?
-            new cs.Invocation(expr2, expr2.slice()) :
-            expr2
-        );
+        if(DEBUG) logExpr(e);
+        if(DEBUG_NATIVE_FUNCS) log(`\n---> READ\n`);
+
+        replace(e, io.read() ? new cs.Invocation(expr2, expr2.slice()) : expr2);
+
         continue;
       }
 
       if(id === 3){
-        log(0);
+        if(DEBUG) logExpr(e);
+        if(DEBUG_NATIVE_FUNCS) log(`\n---> WRITE 0\n`);
+
         io.write(0);
         replace(e, expr2);
+
         continue;
       }
 
       if(id === 4){
-        log(1);
+        if(DEBUG) logExpr(e);
+        if(DEBUG_NATIVE_FUNCS) log(`\n---> WRITE 1\n`);
+
         io.write(1);
         replace(e, expr2);
+
         continue;
       }
     }
 
-    log(expr.toString());
+    if(DEBUG) logExpr();
 
     this.output = io.getOutput();
   }
