842e021d069eb57f2000b3ff118e81f1475c964f
Thomas <hakerh403@gmail.com>
2020-04-04 05:30:32 +0200

    commit

---
+++ test/langs/bip/fibonacci.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const O = require('omikron');
+const esolangs = require('../../..');
+
+module.exports = src => {
+  return [
+    [
+      '',
+      '0\n' +
+      '1\n' +
+      '1\n' +
+      '2\n' +
+      '3\n' +
+      '5\n' +
+      '8\n' +
+      '13\n' +
+      '21\n' +
+      '34\n' +
+      '55\n' +
+      '89\n'
+    ],
+  ];
+};
