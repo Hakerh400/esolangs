5ed138690e17c24bbeee97778099b75bdbb7d2a3
Thomas <hakerh403@gmail.com>
2021-02-25 18:45:25 +0100

    commit

--- src/langs/phd/index.js
+++ src/langs/phd/index.js
@@ -30,8 +30,35 @@ const run = (src, input) => {
     io.write(bit);
   };
 
-  const exec = function*(insts, stack){
+  const sizeMap = new Map();
+
+  const getSize = insts => {
+    if(!sizeMap.has(insts))
+      sizeMap.set(insts, O.rec(calcSize, insts));
+
+    return sizeMap.get(insts);
+  };
+
+  const calcSize = function*(insts){
+    let size = insts.length;
+
+    for(const inst of insts){
+      if(typeof inst === 'number')
+        continue;
+
+      size += yield [calcSize, inst[1]];
+    }
+
+    return size;
+  };
+
+  const exec = function*(insts, stack, test=null){
+    const len = insts.length;
+
     const push = bit => {
+      if(stack.length === 0 && !bit)
+        return;
+
       stack.push(bit);
     };
 
@@ -42,9 +69,29 @@ const run = (src, input) => {
       return stack.pop();
     };
 
-    const len = insts.length;
+    const getSnapshot = () => {
+      assert(test);
+      return [...test.pth, stack.slice(-test.size)];
+    };
+
+    if(test) test.pth.push(0);
 
     for(let i = 0; i !== len; i++){
+      if(test){
+        if(!test.halts) return;
+
+        O.setLast(test.pth, i);
+
+        const snapshot = getSnapshot();
+
+        if(test.map.has(snapshot)){
+          test.halts = 0;
+          return;
+        }
+
+        test.map.set(snapshot, 1);
+      }
+
       const inst = insts[i];
 
       if(typeof inst === 'number'){
@@ -77,18 +124,29 @@ const run = (src, input) => {
 
       if(type === 0){
         while(pop())
-          yield [exec, insts1, stack];
+          yield [exec, insts1, stack, test];
 
         continue;
       }
 
       if(type === 1){
-        esolangs.err(`Instruction () is not implemented yet`);
+        const test = {
+          size: getSize(insts1),
+          map: new O.MultidimensionalMap(),
+          pth: [],
+          halts: 1,
+        };
+
+        yield [exec, insts1, stack.slice(), test];
+        if(test.halts) push(0);
+
         continue;
       }
 
       assert.fail();
     }
+
+    if(test) test.pth.pop();
   };
 
   O.rec(exec, prog, []);
