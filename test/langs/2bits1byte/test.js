2b32076e61fd95f026682c4af1963b72b476a9a0
Thomas <hakerh403@gmail.com>
2021-03-11 13:18:43 +0100

    commit

---
+++ test/langs/2bits1byte/test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const O = require('omikron');
+const esolangs = require('../../..');
+
+module.exports = src => {
+  return [
+    ['', 'm'],
+  ];
+};
